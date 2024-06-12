d77bb835d3c538ac1acead42e4ef0b2819b5c1ba
Luccas Santos <luccasmsantos@outlook.com>
2024-06-12 20:06:12 -0300

    Requisito 01 - Finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    count = 0
+    for start, end in permanence_period:
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+        if start <= target_time <= end:
+            count += 1
+    return count
