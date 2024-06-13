f0a18777b9af3ec9fd3b178d3a9fcccacb1d4742
Luccas Santos <luccasmsantos@outlook.com>
2024-06-13 12:28:25 -0300

    encrypt test done

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,22 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+frase_teste = "abcdef"
+frase_teste_2 = "dcba"
+chave_impar = 3
+chave_par = 4
+chave_negativa = -1
+chave_fora_do_range = 10
+chave_incorreta = "a"
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message(frase_teste, chave_par) == "fe_dcba"
+    assert encrypt_message(frase_teste, chave_impar) == "cba_fed"
+
+    assert encrypt_message(frase_teste, chave_negativa) == "fedcba"
+
+    with pytest.raises(TypeError):
+        encrypt_message(frase_teste, chave_incorreta)
+
+    assert encrypt_message(frase_teste_2, 2) == "ab_cd"
